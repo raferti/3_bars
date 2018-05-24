523d4d393337d8e95300cd30a028683ac4a2b4eb
raferti <raferti712@yandex.ru>
2018-05-24 15:55:28 +0300

    Правка форматирования

--- bars.py
+++ bars.py
@@ -96,7 +96,7 @@ if __name__ == '__main__':
                                  longitude,
                                  latitude
                              )
-                         )
+                            )
                          )
           )
 
